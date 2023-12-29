e9da74124795589418e0806e99d6084741653b2e
EdherAgaton <115425293+EdherAgaton@users.noreply.github.com>
2023-12-29 17:22:34 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ app.use(express.urlencoded({extended : true  }));
 
 //
 app.use(require('./routes/usersRoutes'))
+app.use(require('./routes/gerentesRoutes'))
 
 app.listen(3000);
 console.log('Servidor en el puerto 3000');
