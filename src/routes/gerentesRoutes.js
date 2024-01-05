17285a75d6ba5d3ac009591e468953fa86f292a4
EdherAgaton <115425293+EdherAgaton@users.noreply.github.com>
2024-01-05 12:54:46 -0600

    Update gerentesRoutes.js

--- src/routes/gerentesRoutes.js
+++ src/routes/gerentesRoutes.js
@@ -1,10 +1,11 @@
 const {Router} = require('express')
 const router = Router()
 
-const { updateCortes, viewCortes } = require('../controller/gerenteController')
+const { updateCortes, viewCortes, postDeposito } = require('../controller/gerenteController')
 
 router.post('/gerente/cortes/ver', viewCortes)
 router.post('/gerente/cortes/modificar', updateCortes)
+router.post('/gerente/cortes/deposito', postDeposito)
 
 module.exports = router
 
